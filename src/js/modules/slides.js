1511a2e730a8f7eb29c6c9dfa6a4f821b583bff0
Pedro Britto <pedroivobritto@gmail.com>
2019-06-18 22:31:39 -0300

    Add slide de produtos relacionados

--- src/js/modules/slides.js
+++ src/js/modules/slides.js
@@ -5,5 +5,26 @@ Swiper.use([Navigation, Pagination]);
 export default () => {
     // Instâncias do Swiper aqui...
 
-    new Swiper(".swiper-container");
+    new Swiper(".swiper-related-products", {
+        breakpointsInverse: true,
+        slidesPerView: 2,
+        navigation: {
+            nextEl: ".swiper-nav-next",
+            prevEl: ".swiper-nav-prev",
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 3,
+            },
+            600: {
+                slidesPerView: 4,
+            },
+            800: {
+                slidesPerView: 5,
+            },
+            900: {
+                slidesPerView: 6,
+            },
+        },
+    });
 };

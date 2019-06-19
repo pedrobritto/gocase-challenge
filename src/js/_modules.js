8f32050797c9e4e3185be6bc793396c17d3b7d40
Pedro Britto <pedroivobritto@gmail.com>
2019-06-18 23:01:59 -0300

    Ajustes no header e menu mobile

--- src/js/_modules.js
+++ src/js/_modules.js
@@ -1,8 +1,7 @@
 import burger from "./modules/burger";
 import FixedElementClearance from "./modules/FixedElementClearance";
 import LazyLoad from "vanilla-lazyload";
-// import MenuToggle from "./modules/MenuToggle";
-// import SmoothScroll from "smooth-scroll/dist/smooth-scroll";
+import MenuToggle from "./modules/MenuToggle";
 
 import Modal from "./modules/ExiaModal";
 import slides from "./modules/slides";
@@ -13,25 +12,18 @@ export default () => {
 
     new LazyLoad({ elements_selector: ".lazy" });
 
-    // new SmoothScroll('a[href*="#"]', {
-    //     header: ".main-header-bundle",
-    //     speed: 1500,
-    //     speedAsDuration: true,
-    //     easing: "easeOutQuint",
-    // });
-
     new FixedElementClearance({
         element: ".js-get-main-header-height",
         CSSVariableName: "main-header-height",
     });
 
-    // new MenuToggle({
-    //     menuToggle: [".js-main-menu-mobile-toggle"],
-    //     menuElement: ".main-menu-mobile",
-    //     menuClose: [".js-close-menu-mobile"],
-    //     activeBodyClass: "mobile-menu-is-open",
-    //     breakpointToHide: 1200,
-    // });
+    new MenuToggle({
+        menuToggle: [".js-main-menu-mobile-toggle"],
+        menuElement: ".main-menu-mobile",
+        menuClose: [".js-close-menu-mobile"],
+        activeBodyClass: "mobile-menu-is-open",
+        breakpointToHide: 1200,
+    });
 
     new Modal();
 };
